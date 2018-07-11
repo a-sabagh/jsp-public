b733488930e4f896b9e55be186b8763e2b2d8b6f
asabagh <a.sabagh72@gmail.com>
2018-07-11 15:16:05 -0700

    5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+	/*menu-toggl*/
+	$(".mobile-toggle").on("click",function(e){
+		e.preventDefault();
+		$(this).next("ul").slideToggle();
+	});
+	$("#main-nav > ul > li.has-child a").on("click",function(e){
+		e.preventDefault();
+		$(this).next(".sub-menu-wrapper").slideToggle();
+	});
 	/* slick slider */
 	$(".slick-slider").slick({
 		rtl: true,
@@ -35,20 +44,20 @@ $(document).ready(function(){
 			{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
+					slidesToShow: 2,
+					slidesToScroll: 2,
 					infinite: true,
 				}
 			},
 			{
-				breakpoint: 600,
+				breakpoint: 768,
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 2
 				}
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 550,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1
